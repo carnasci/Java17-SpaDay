a1cba03772db705a814a57cec1cff7755bd0cdf2
carnasci <cen.master24@gmail.com>
2024-01-25 21:16:24 -0600

    spa day user sign up finished

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -10,16 +10,23 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("add")
-    public String displayUserForm() {
+    public String displayUserForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("username", user.getUsername());
         return "user/add";
     }
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify, String email,
+                                     String username) {
         model.addAttribute("verify", verify);
+
         if (user.getPassword().equals(verify)) {
             model.addAttribute(user);
             return "/user/index.html";
         } else {
+            model.addAttribute("error", "Password not verified!");
             return "/user/add";
         }
 
